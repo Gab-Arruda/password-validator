0fda96066aac97cd4ba29cd3bbbc2e55acd1ade5
gabriel.santos <gabriel.santos@vegamonitoramento.com.br>
2026-01-29 18:51:36 -0300

    Inicializacao do projeto password-validator

---
+++ src/test/java/com/gabriel_arruda/password_validator/PasswordValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_arruda.password_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
